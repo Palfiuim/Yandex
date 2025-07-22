4cb7832bd8c61a220bc0c18910d473c33e9b2861
Anton Grishin <AGrishin29@yandex.ru>
2025-07-22 16:31:33 +0300

    Create configuration.py

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Антон Гришин,  — Финальный проект. Инженер по тестированию плюс
+
+URL_SERVICE = "https://d8400740-7698-4211-9f53-9f124c75423b.serverhub.praktikum-services.ru"
+CREAT_ORDERS = "/api/v1/orders"
