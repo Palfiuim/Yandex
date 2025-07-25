7b5503ddf15393e762f091f91b90bc8549a904d9
Anton Grishin <AGrishin29@yandex.ru>
2025-07-25 19:38:20 +0300

    Create order_creation_test.py

---
+++ order_creation_test.py
@@ -0,0 +1,10 @@
+from sendor_stand_request import create_order, get_order_by_track
+import data
+
+def test_order_creation():
+    response = create_order(data.order_body)
+    assert response.status_code == 201
+
+    track = response.json()["track"]
+    order_response = get_order_by_track(track)
+    assert order_response.status_code == 200
